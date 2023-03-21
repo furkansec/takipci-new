4964ad0145dc5f2681bb5d01a62627a0f3fc545e
FURKAN <121892854+swantex@users.noreply.github.com>
2023-03-21 21:29:15 +0300

    Create baslat.py

---
+++ baslat.py
@@ -0,0 +1,50 @@
+import pyfiglet
+import rich
+import random
+
+from rich.console import Console
+from rich.prompt import Prompt
+
+console = Console()
+
+banner = pyfiglet.figlet_format("TAKIPCI", font="slant")
+console.print(
+   f"[bold red]{banner}",
+   "Creator: swantex",
+   "Channel: @öztürk yalçın",
+
+   sep = "\n",
+   style = "bold white"
+)
+
+
+def BannerGeneration():
+
+    banner_text = console.input("[bold red]\Kullanıcı adı girin[/]> ")
+    
+    get_fonts = pyfiglet.FigletFont.getFonts()
+    
+    try:
+       count = 0
+       
+       many = Prompt.ask(
+          "[bold red]TAKİPÇİ SAYISI",
+          MAX="10.OOO"
+       )
+
+       while count < int(many):
+             random_fonts = random.choice(get_fonts)
+             
+             banners = pyfiglet.figlet_format(banner_text, font=random_fonts)
+
+             count += 1
+             console.print(f"[bold magenta]TAKİPCİ BASARIYLA GÖNDERİLDİ[/]: [bold white][{count}]")
+
+             file = open("banners.txt", "a")
+             file.write(f"{banners}")
+             file.close()
+             
+    except:
+          console.print("[bold red][!] olmadı")
+
+BannerGeneration()
